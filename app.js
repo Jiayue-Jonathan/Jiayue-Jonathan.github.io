1b5891bf0c25568e1ef883bcd1751bf876dcde8e
Jiayue-Jonathan <jonathanlv201710@gmail.com>
2024-02-14 21:32:10 -0800

    New assignment

---
+++ app.js
@@ -0,0 +1,19 @@
+//app.js
+
+'use strict';
+
+const switcher= document.querySelector('.btn');
+switcher.addEventListener('click',function(){
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+const className= document.body.className;
+
+if(className =="light-theme"){
+    switcher.textContent="Dark";
+}else{
+    switcher.textContent="Light";
+}
+
+});
+
